e9a69c6c50578868c543b865887e2bffba836c7f
minee95 <minervaestela0495@gmail.com>
2022-12-08 20:02:52 -0500

    texto del libro con leer mas y quitar de la pagina principal vainas innecesarias

--- SC-Minerva/js/main.js
+++ SC-Minerva/js/main.js
@@ -1,3 +1,20 @@
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Leer más";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Leer menos";
+      moreText.style.display = "inline";
+    }
+  }
+
+
 jQuery(document).ready(function($) {
 
     'use strict';
